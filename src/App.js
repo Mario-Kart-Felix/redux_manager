526694b35a5912534a2c7ae7334677805b8e3c1d
BandanaPandey <bandanapandey11@gmail.com>
2019-07-28 18:44:16 +0530

    Created login form using redux and firebase

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { View, Text } from 'react-native';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
 class App extends Component{
 	componentDidMount(){
 			const firebaseConfig = {
-		    apiKey: 'AIzaSyDlcQy6qjMBjuw90Lt0hMqfHT4mE37iXmE',
-		    authDomain: 'authentication-491ab.firebaseapp.com',
-		    databaseURL: 'https://authentication-491ab.firebaseio.com',
-		    projectId: 'authentication-491ab',
-		    storageBucket: '',
-		    messagingSenderId: '1047052287027',
-		    appId: '1:1047052287027:web:763eb3b3896429d1'
+		    apiKey: "AIzaSyDnrYpUVNiZMvEX6--HO4hwksvvqmr3UIg",
+			  authDomain: "manager-76dcd.firebaseapp.com",
+			  databaseURL: "https://manager-76dcd.firebaseio.com",
+			  projectId: "manager-76dcd",
+			  storageBucket: "",
+			  messagingSenderId: "569505619762",
+			  appId: "1:569505619762:web:0dedc4f08789e34d"
 	  };
 	  // Initialize Firebase
 	  firebase.initializeApp(firebaseConfig);
 	}
 	render(){
+		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 		return(
-				<Provider store={createStore(reducers)}>
-					<View>
-						<LoginForm></LoginForm>
-					</View>
+				<Provider store={store}>
+					<LoginForm></LoginForm>
 				</Provider>
 			);
 	}
